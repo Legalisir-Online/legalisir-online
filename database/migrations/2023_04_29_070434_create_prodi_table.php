da431357a0453493f42147f28cc0ee6050a8f92a
bluaeri <realnabluechan@gmail.com>
2023-04-29 18:56:48 +0700

    all user & prodi

---
+++ database/migrations/2023_04_29_070434_create_prodi_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prodi', function (Blueprint $table) {
+            $table->string('kode', 2)->primary();
+            $table->string('nama_prodi', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prodi');
+    }
+};
