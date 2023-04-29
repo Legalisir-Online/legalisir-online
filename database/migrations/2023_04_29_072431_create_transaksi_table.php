29a8805312ca4370f10601e640908eb0113753ae
bluaeri <realnabluechan@gmail.com>
2023-04-29 18:57:55 +0700

    tabel dokumen & transaksi

---
+++ database/migrations/2023_04_29_072431_create_transaksi_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_alumni');
+            $table->foreign('id_alumni')->references('id')->on('alumni');
+            $table->integer('jumlah_tagihan');
+            $table->string('status_transaksi', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
