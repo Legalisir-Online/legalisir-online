518bd7a74cb09a8e3e47d2c2bd080a910633d193
bluaeri <realnabluechan@gmail.com>
2023-05-04 11:09:14 +0700

    konfirmasi password

---
+++ resources/views/konfirm-pw.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sistem Legalisir Online</title>
+    <link rel="stylesheet" href="{{ asset('css/logindkk.css') }}">
+</head>
+<body>
+    <div class="logo">
+        <a href="/"><img src="{{ asset('images/logo.png') }}" alt="Logo"></a>
+    </div>
+    <div class="container">
+        <img src="{{ asset('images/ppl.png') }}" alt="ppl">
+        <div class="overlay1">
+            <p>&emsp;Reset Password</p>
+        </div>
+        <div class="login-box">
+            <h2>Reset Password</h2>
+            <form action="" method="POST">
+                
+                <div class="form-group">
+                    <input type="password" name="new-password" id="new-password" placeholder="password baru" required>
+                </div>
+                <div class="form-group">
+                    <input type="password" name="konfirm-password" id="konfirm-password" placeholder="konfirmasi password" required>
+                </div>
+                <div class="form-group">
+                    <button type="submit">Reset</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/konfirm-pw', function () {
+    return view('konfirm-pw');
+});
