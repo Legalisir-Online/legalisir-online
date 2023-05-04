0ccd11de1d0f0ea475593431e7b0729c3197daf7
Ardhyla Dwi SP <ardhyla@student.uns.ac.id>
2023-05-04 22:50:34 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/form-legalisir', function () {
+    return view('form-legalisir');
+});
