3df96ca6e8b5e12c4e51eacbeaf9408d71f8a45a
bachtiarashidiqy <bachtiar.asyidiqy@gmail.com>
2023-05-04 18:30:57 +0700

    update layouting

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,39 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Sistem Legalisir Online</title>
     <link rel="stylesheet" href="{{ asset('css/logindkk.css') }}">
 </head>
+
 <body>
     <div class="logo">
         <a href="/"><img src="{{ asset('images/logo.png') }}" alt="Logo"></a>
     </div>
     <div class="container">
-        <img src="{{ asset('images/ppl.png') }}" alt="ppl">
-        <div class="overlay1">
-            <p>&emsp;Log In</p>
-        </div>
-        <div class="overlay2">
-            <p>&emsp;Register</p>
+        <div class="image-wrapper">
+            <img src="{{ asset('images/ppl.png') }}" alt="ppl">
+            <div class="group-overlay">
+                <div class="overlay overlay-active">
+                    <p>Log In</p>
+                </div>
+                <div class="overlay">
+                    <p>Register</p>
+                </div>
+            </div>
         </div>
         <div class="login-box">
-            <h2>Login Account</h2>
-            <form action="" method="POST">
-                
-                <div class="form-group">
-                    <input type="email" name="email" id="email" placeholder="email" required>
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password" id="password" placeholder="password" required>
-                </div>
-                <div class="form-group">
-                    <a href="#"><p>lupa password?</p></a>
-                </div>
-                <div class="form-group">
-                    <button type="submit">Login</button>
-                </div>
-            </form>
+
+            <div class="form">
+                <h2>Login Account</h2>
+                <form action="" method="POST">
+
+                    <div class="form-group">
+                        <input type="email" name="email" id="email" placeholder="email" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" id="password" placeholder="password" required>
+                    </div>
+                    <div class="form-group">
+                        <a href="#">
+                            <p>lupa password?</p>
+                        </a>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,36 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Sistem Legalisir Online</title>
     <link rel="stylesheet" href="{{ asset('css/logindkk.css') }}">
 </head>
+
 <body>
     <div class="logo">
         <a href="/"><img src="{{ asset('images/logo.png') }}" alt="Logo"></a>
     </div>
     <div class="container">
-        <img src="{{ asset('images/ppl.png') }}" alt="ppl">
+        <div class="image-wrapper">
+            <img src="{{ asset('images/ppl.png') }}" alt="ppl">
+            <div class="group-overlay">
+                <div class="overlay">
+                    <p>Log In</p>
+                </div>
+                <div class="overlay overlay-active">
+                    <p>Register</p>
+                </div>
+            </div>
+        </div>
         <div class="login-box">
-            <h2>Register Account</h2>
-            <form action="" method="POST">
-                
-                <div class="form-group">
-                    <input type="text" name="nim" id="nim" placeholder="NIM" required>
-                </div>
-                <div class="form-group">
-                    <input type="email" name="email" id="email" placeholder="email" required>
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password" id="password" placeholder="password" required>
-                </div>
-                <div class="form-group">
-                    <input type="date" name="date" id="date" placeholder="Tanggal Lahir" required>
-                </div>
-                <div class="form-group">
-                    <button type="submit">Register</button>
-                </div>
-            </form>
+            <div class="form">
+                <h2>Register Account</h2>
+                <form action="" method="POST">
+
+                    <div class="form-group">
+                        <input type="text" name="nim" id="nim" placeholder="NIM" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="email" id="email" placeholder="email" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" id="password" placeholder="password" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="date" name="date" id="date" placeholder="Tanggal Lahir" required>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
