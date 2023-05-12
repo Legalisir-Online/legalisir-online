cdd3b79e938f0cfa53d1978671d96c54ef28c1e2
Della Kornelya Putri <dellakornel@student.uns.ac.id>
2023-05-12 07:11:34 +0700

    kuisioner belum selesai

---
+++ resources/views/kuisioner.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Sistem Legalisir Online</title>
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/kuisioner.css') }}">
+</head>
+
+<body>
+  <nav class="navbar">
+    <div class="navbar-logo">
+      <a href="/riwayat-ajuan"><img src="{{ asset('images/logo.png') }}" alt="Logo"></a>
+    </div>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a href="#" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">Ajuan</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">Profile</a>
+      </li>
+    </ul>
+  </nav>
+
+  <div class="container">
+    <div class="container-head">
+      Isi Kuisioner
+    </div>
+    <div>
+      
+    </div>
+  </div>
+
+</body>
+
+<footer class="footer">
+    <div class="footer-text">
+        &copy; Fakultas Teknologi Informasi Dan Sains Data UNS - 2023
+    </div>
+</footer>
+
+</html>
