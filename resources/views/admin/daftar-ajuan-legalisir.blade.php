6ae3fcd35f4d27e11f23bf703e964634bd72a5e3
bachtiarashidiqy <bachtiar.asyidiqy@gmail.com>
2023-05-18 18:21:15 +0700

    Fix table in dashboard

--- resources/views/admin/daftar-ajuan-legalisir.blade.php
+++ resources/views/admin/daftar-ajuan-legalisir.blade.php
@@ -20,87 +20,97 @@
         </div>
     </section>
 
-    <div style="margin-top: 10px" class="container">
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-xs-12">
-                <div class="box">
-                    <div class="box-tools">
-                        <div class="input-group">
-                            <input type="text" name="table_search" class="form-control input-sm pull-right"
-                                style="width: 150px;" placeholder="Search" />
-                            <div class="input-group-btn">
-                                <button class="btn btn-sm btn-primary"><i class="fa fa-search"></i></button>
-                            </div>
-                        </div>
-                    </div><!-- /.box-header -->
-                    <div class="box-body table-responsive no-padding">
-                        <table class="table table-hover">
-                            <tr>
-                                <th style="background-color: black; color: white;">No</th>
-                                <th style="background-color: black; color: white;">Nama</th>
-                                <th style="background-color: black; color: white;">NIM</th>
-                                <th style="background-color: black; color: white;">Status</th>
-                                <th style="background-color: black; color: white;">Keterangan</th>
-                                <th style="background-color: black; color: white;">Pembayaran</th>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>John Doe</td>
-                                <td>M0520001</td>
-                                <td><span class="label label-success">Passed</span></td>
-                                <td>
-                                    <span><a><button class="btn btn-primary">Detail</button></a></span>
-                                    <span><a><button class="btn btn-success">Edit</button></a></span>
-                                </td>
-                                <td><span class="label label-info">lunas</span></td>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>John Doe</td>
-                                <td>M0520001</td>
-                                <td><span class="label label-success">Passed</span></td>
-                                <td>
-                                    <span><a><button class="btn btn-primary">Detail</button></a></span>
-                                    <span><a><button class="btn btn-success">Edit</button></a></span>
-                                </td>
-                                <td><span class="label label-info">lunas</span></td>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>John Doe</td>
-                                <td>M0520001</td>
-                                <td><span class="label label-success">Passed</span></td>
-                                <td>
-                                    <span><a><button class="btn btn-primary">Detail</button></a></span>
-                                    <span><a><button class="btn btn-success">Edit</button></a></span>
-                                </td>
-                                <td><span class="label label-info">lunas</span></td>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>John Doe</td>
-                                <td>M0520001</td>
-                                <td><span class="label label-success">Passed</span></td>
-                                <td>
-                                    <span><a><button class="btn btn-primary">Detail</button></a></span>
-                                    <span><a><button class="btn btn-success">Edit</button></a></span>
-                                </td>
-                                <td><span class="label label-info">lunas</span></td>
-                            </tr>
-                        </table>
-                    </div><!-- /.box-body -->
-                    <div class="text-center">
-                        <nav aria-label="Page navigation">
-                            <ul class="pagination justify-content-center">
-                                <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
-                            </ul>
-                        </nav>
+                <div class="input-group">
+                    <input type="text" name="table_search" class="form-control input-sm pull-right"
+                        style="width: 150px;" placeholder="Search" />
+                    <div class="input-group-btn">
+                        <button class="btn btn-sm btn-primary"><i class="fa fa-search"></i></button>
                     </div>
-                </div><!-- /.box -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="content">
+        <div style="margin-top: 10px" class="container">
+            <div class="row justify-content-center">
+                <div class="col-xs-12">
+                    <div class="box">
+                        <div class="box-body table-responsive no-padding">
+                            <table class="table table-hover">
+                                <tr>
+                                    <th style="background-color: black; color: white; width: 10%; padding-left: 25px;">
+                                        No
+                                    </th>
+                                    <th style="background-color: black; color: white; width: 25%;">Nama</th>
+                                    <th style="background-color: black; color: white; width: 15%;">NIM</th>
+                                    <th style="background-color: black; color: white; width: 15%;">Status</th>
+                                    <th style="background-color: black; color: white; width: 20%;">Keterangan</th>
+                                    <th style="background-color: black; color: white; width: 15%;">Pembayaran</th>
+                                </tr>
+                                <tr>
+                                    <td style="padding-left: 25px;">1</td>
+                                    <td>John Doe</td>
+                                    <td>M0520001</td>
+                                    <td><span class="label label-success">Passed</span></td>
+                                    <td>
+                                        <span><a><button class="btn btn-primary">Detail</button></a></span>
+                                        <span><a><button class="btn btn-success">Edit</button></a></span>
+                                    </td>
+                                    <td><span class="label label-info">lunas</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="padding-left: 25px;">1</td>
+                                    <td>John Doe</td>
+                                    <td>M0520001</td>
+                                    <td><span class="label label-success">Passed</span></td>
+                                    <td>
+                                        <span><a><button class="btn btn-primary">Detail</button></a></span>
+                                        <span><a><button class="btn btn-success">Edit</button></a></span>
+                                    </td>
+                                    <td><span class="label label-info">lunas</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="padding-left: 25px;">1</td>
+                                    <td>John Doe</td>
+                                    <td>M0520001</td>
+                                    <td><span class="label label-success">Passed</span></td>
+                                    <td>
+                                        <span><a><button class="btn btn-primary">Detail</button></a></span>
+                                        <span><a><button class="btn btn-success">Edit</button></a></span>
+                                    </td>
+                                    <td><span class="label label-info">lunas</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="padding-left: 25px;">1</td>
+                                    <td>John Doe</td>
+                                    <td>M0520001</td>
+                                    <td><span class="label label-success">Passed</span></td>
+                                    <td>
+                                        <span><a><button class="btn btn-primary">Detail</button></a></span>
+                                        <span><a><button class="btn btn-success">Edit</button></a></span>
+                                    </td>
+                                    <td><span class="label label-info">lunas</span></td>
+                                </tr>
+                            </table>
+                        </div><!-- /.box-body -->
+                        <div class="text-center">
+                            <nav aria-label="Page navigation">
+                                <ul class="pagination justify-content-center">
+                                    <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div><!-- /.box -->
+                </div>
             </div>
         </div>
     </section>
