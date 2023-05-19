e3b4818f21a2ac7bbbdd7287cb24aa13efce0296
Theo <theo@student.uns.ac.id>
2023-05-19 09:32:19 +0700

    Tambah radio button kuesioner

--- resources/views/kuisioner.blade.php
+++ resources/views/kuisioner.blade.php
@@ -17,29 +17,61 @@
         <a href="#" class="nav-link">Home</a>
       </li>
       <li class="nav-item dropdown">
-				<a href="#" class="nav-link dropdown-toggle">Ajuan</a>
-				<ul class="dropdown-menu">
-					<li><a href="/status-ajuan1">Status Ajuan</a></li>
-					<li><a href="/riwayat-ajuan">Riwayat Ajuan</a></li>
-					<li><a href="/invoice">Invoice</a></li>
-				</ul>
-			</li>
-			<li class="nav-item dropdown">
-				<a href="#" class="nav-link dropdown-toggle">Profile</a>
-				<ul class="dropdown-menu">
-					<li><a href="/biodata">Biodata</a></li>
-					<li><a href="/kuisioner">Kuisioner</a></li>
-					<li><a href="/login">Logout</a></li>
-				</ul>
-			</li>
+        <a href="#" class="nav-link dropdown-toggle">Ajuan</a>
+        <ul class="dropdown-menu">
+          <li><a href="/status-ajuan1">Status Ajuan</a></li>
+          <li><a href="/riwayat-ajuan">Riwayat Ajuan</a></li>
+          <li><a href="/invoice">Invoice</a></li>
+        </ul>
+      </li>
+      <li class="nav-item dropdown">
+        <a href="#" class="nav-link dropdown-toggle">Profile</a>
+        <ul class="dropdown-menu">
+          <li><a href="/biodata">Biodata</a></li>
+          <li><a href="/kuisioner">Kuisioner</a></li>
+          <li><a href="/login">Logout</a></li>
+        </ul>
+      </li>
     </ul>
   </nav>
 
   <div class="container">
     <div class="container-head">
-        Isi Kuisioner
+      Isi Kuisioner
     </div> <br> <br>
-    
+
+    <div class="content">
+      <div class="content-pertanyaan">
+        Menurut anda, apakah sistem pelayanan mahasiswa alumni yang diberikan oleh Fakultas Teknologi Informasi dan Sains Data sudah memuaskan?
+      </div>
+      <div class="content-essay">
+        <ul>
+          <li><label for="radio-1">Sangat tidak Setuju</label> <input type="radio" name="opinions" id="radio-1" /> </li>
+          <li><label for="radio-2">Tidak setuju</label> <input type="radio" name="opinions" id="radio-2" /> </li>
+          <li><label for="radio-3">Netral</label> <input type="radio" name="opinions" id="radio-3" /> </li>
+          <li><label for="radio-4">Setuju</label> <input type="radio" name="opinions" id="radio-4" /> </li>
+          <li><label for="radio-5">Sangat setuju</label> <input type="radio" name="opinions" id="radio-5" /> </li>
+          <ul>
+            <!-- <input type="text" class="content-essay" placeholder="Jawaban"> -->
+      </div>
+    </div>
+    <br>
+    <div class="content">
+      <div class="content-pertanyaan">
+        Menurut anda, apakah sistem pelayanan mahasiswa alumni yang diberikan oleh Fakultas Teknologi Informasi dan Sains Data sudah memuaskan?
+      </div>
+      <div class="content-essay">
+        <ul>
+          <li><label for="radio-1">Sangat tidak Setuju</label> <input type="radio" name="opinions" id="radio-1" /> </li>
+          <li><label for="radio-2">Tidak setuju</label> <input type="radio" name="opinions" id="radio-2" /> </li>
+          <li><label for="radio-3">Netral</label> <input type="radio" name="opinions" id="radio-3" /> </li>
+          <li><label for="radio-4">Setuju</label> <input type="radio" name="opinions" id="radio-4" /> </li>
+          <li><label for="radio-5">Sangat setuju</label> <input type="radio" name="opinions" id="radio-5" /> </li>
+          <ul>
+            <!-- <input type="text" class="content-essay" placeholder="Jawaban"> -->
+      </div>
+    </div>
+    <br>
     <div class="content">
       <div class="content-pertanyaan">
         Menurut anda, apakah sistem pelayanan mahasiswa alumni yang diberikan oleh Fakultas Teknologi Informasi dan Sains Data sudah memuaskan?
@@ -57,7 +89,8 @@
         <input type="text" class="content-essay" placeholder="Jawaban">
       </div>
     </div>
-    
+
+
     <!-- <div class="selesai">
       <button>Selesai</button>
     </div> -->
@@ -83,9 +116,9 @@
 </body>
 
 <footer class="footer">
-    <div class="footer-text">
-        &copy; Fakultas Teknologi Informasi Dan Sains Data UNS - 2023
-    </div>
+  <div class="footer-text">
+    &copy; Fakultas Teknologi Informasi Dan Sains Data UNS - 2023
+  </div>
 </footer>
 
 </html>
