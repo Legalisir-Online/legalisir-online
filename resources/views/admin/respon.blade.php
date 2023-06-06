acf0ba6032d2cd1792a94ef7291dc8a46200942f
bluaeri <realnabluechan@gmail.com>
2023-06-06 21:01:31 +0700

    add pertanyaan baru & respon #blm jadi#

---
+++ resources/views/admin/respon.blade.php
@@ -0,0 +1,88 @@
+@include('admin.includes.header')
+@include('admin.includes.topbar')
+@include('admin.includes.sidebar')
+
+<div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-xs-12">
+                            <h1>
+                                Respon
+                            </h1>
+                            <ol class="breadcrumb">
+                                <li><a href="/admin"><i class="fa fa-dashboard"></i> Home</a></li>
+                                <li>Kuesioner</li>
+                                <li class="active">Respon</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+    <div class="content">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xs-12">
+                <div class="box" style="background: #D9D9D9; border-radius: 10px;">
+                <div class="row" style="width: 92%; margin:auto;">
+                    <br>
+                    <table>
+                        <tr>
+                            <td style="padding-right: 25px;"><b>Kuesioner</b></td>
+                            <td>: [Nama Kuesioner]</td>
+                        </tr>
+                        <tr>
+                            <td style="padding-right: 25px;"><b>Periode </td>
+                            <td>: [Periode]</td>
+                        </tr>
+                        <tr>
+                            <td style="padding-right: 25px;"><b>Status </td>
+                            <td>: Aktif / Tidak Aktif</td>
+                        </tr>
+                        <tr>
+                            <td style="padding-right: 25px;"><b>Hasil Respon </td>
+                            <td>: 15 Responden</td>
+                        </tr>
+                    </table><br>
+                </div>
+                </div> 
+                <div class="box" style="background: #D9D9D9; border-radius: 10px;">
+                <div class="row" style="width: 92%; margin:auto;">
+                    <br>
+                    <span><b>Nama</b></span>
+                    <p>15 Responden</p>
+                    <br>
+                </div>
+                </div> 
+                <div class="box" style="background: #D9D9D9; border-radius: 10px;">
+                <div class="row" style="width: 92%; margin:auto;">
+                    <br>
+                    <span><b>NIM</b></span>
+                    <p>15 Responden</p>
+                    <br>
+                </div>
+                </div> 
+                <div class="box" style="background: #D9D9D9; border-radius: 10px;">
+                <div class="row" style="width: 92%; margin:auto;">
+                    <br>
+                    <span><b>Menurut anda, apakah sistem pelayanan mahasiswa alumni yang diberikan oleh Fakultas Teknologi Informasi dan Sains Data sudah memuaskan?</b></span>
+                    <p>15 Responden</p>
+                    <br>
+                </div>
+                </div> 
+                <div class="box" style="background: #D9D9D9; border-radius: 10px;">
+                <div class="row" style="width: 92%; margin:auto;">
+                    <br>
+                    <span><b>Menurut anda, apakah sistem pelayanan mahasiswa alumni yang diberikan oleh Fakultas Teknologi Informasi dan Sains Data sudah memuaskan?</b></span>
+                    <p>15 Responden</p>
+                    <br>
+                </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+    </div>
+
+@include('admin.includes.footer')
