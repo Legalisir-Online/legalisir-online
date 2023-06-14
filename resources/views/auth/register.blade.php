53cb7feacbcb749a0e3deef04941948e64d47171
bachtiarashidiqy <bachtiar.asyidiqy@gmail.com>
2023-06-14 23:18:27 +0700

    Fix Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,97 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <meta charset="UTF-8">
+    <title>Sistem Legalisir Online</title>
+    <link rel="stylesheet" href="{{ asset('css/logindkk.css') }}">
+</head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<body>
+    <div class="logo">
+        <a href="/"><img src="{{ asset('images/logo.png') }}" alt="Logo"></a>
+    </div>
+    <div class="container">
+        <div class="image-wrapper">
+            <img src="{{ asset('images/ppl.png') }}" alt="ppl">
+            <div class="group-overlay">
+                <div class="overlay">
+                    <a href="/login">
+                        <p>Log In</p>
+                    </a>
+                </div>
+                <div class="overlay overlay-active">
+                    <a href="/register">
+                        <p>Register</p>
+                    </a>
                 </div>
             </div>
         </div>
+        <div class="login-box">
+            <div class="form">
+                <h2>{{ __('Register Account') }}</h2>
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <div class="form-group">
+                        <input id="nim" type="text" class="form-control @error('nim') is-invalid @enderror"
+                            name="nim" value="{{ old('nim') }}" required autocomplete="nim" placeholder="NIM"
+                            autofocus>
+
+                        @error('nim')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" required placeholder="email"
+                            autocomplete="email">
+
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <input id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password" required
+                            placeholder="password" autocomplete="new-password">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <input id="date" type="date" class="form-control @error('date') is-invalid @enderror"
+                            name="date" required placeholder="Tanggal Lahir" placeholder="Tanggal Lahir">
+
+                        @error('date')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    {!! NoCaptcha::renderJs() !!}
+                    {!! NoCaptcha::display() !!}
+                    <div class="form-group">
+                        <button type="submit"><a href="/akun-terverifikasi">{{ __('Register') }}</a></button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,188 +13,186 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('landing-page');
-// });
+Route::get('/', function () {
+    return view('landing-page');
+});
 
-// Route::get('/login', function () {
-//     return view('login');
-// });
+Route::get('/login', function () {
+    return view('login');
+});
 
-// Route::get('/reset-password', function () {
-//     return view('reset-password');
-// });
+Route::get('/reset-password', function () {
+    return view('reset-password');
+});
 
-// Route::get('/forgot-password', function () {
-//     return view('forgot-password');
-// });
+Route::get('/forgot-password', function () {
+    return view('forgot-password');
+});
 
-// Route::get('/register', function () {
-//     return view('register');
-// });
+Route::get('/register', function () {
+    return view('register');
+});
 
-// Route::get('/biodata', function () {
-//     return view('alumni/biodata');
-// });
+Route::get('/biodata', function () {
+    return view('alumni/biodata');
+});
 
-// Route::get('/profile', function () {
-//     return view('alumni/profile');
-// });
+Route::get('/profile', function () {
+    return view('alumni/profile');
+});
 
-// Route::get('/upload-berkas', function () {
-//     return view('alumni/upload-berkas');
-// });
+Route::get('/upload-berkas', function () {
+    return view('alumni/upload-berkas');
+});
 
-// Route::get('/akun-terverifikasi', function () {
-//     return view('aktivasi-akun');
-// });
+Route::get('/akun-terverifikasi', function () {
+    return view('aktivasi-akun');
+});
 
-// Route::get('/homepage', function () {
-//     return view('alumni/homepage');
-// });
+Route::get('/homepage', function () {
+    return view('alumni/homepage');
+});
 
-// Route::get('/form-legalisir', function () {
-//     return view('alumni/form-legalisir');
-// });
+Route::get('/form-legalisir', function () {
+    return view('alumni/form-legalisir');
+});
 
-// Route::get('/status-ajuan1', function () {
-//     return view('alumni/status-ajuan1');
-// });
+Route::get('/status-ajuan1', function () {
+    return view('alumni/status-ajuan1');
+});
 
-// Route::get('/riwayat-ajuan', function () {
-//     return view('alumni/riwayat-ajuan');
-// });
+Route::get('/riwayat-ajuan', function () {
+    return view('alumni/riwayat-ajuan');
+});
 
-// Route::get('/invoice', function () {
-//     return view('alumni/invoice');
-// });
+Route::get('/invoice', function () {
+    return view('alumni/invoice');
+});
 
-// Route::get('/status-ajuan2', function () {
-//     return view('alumni/status-ajuan2');
-// });
+Route::get('/status-ajuan2', function () {
+    return view('alumni/status-ajuan2');
+});
 
-// Route::get('/riwayat-invoice', function () {
-//     return view('alumni/riwayat-invoice');
-// });
+Route::get('/riwayat-invoice', function () {
+    return view('alumni/riwayat-invoice');
+});
 
-// Route::get('/kuesioner', function () {
-//     return view('alumni/kuesioner');
-// });
+Route::get('/kuesioner', function () {
+    return view('alumni/kuesioner');
+});
 
-// Route::get('/status-ajuan3', function () {
-//     return view('alumni/status-ajuan3');
-// });
+Route::get('/status-ajuan3', function () {
+    return view('alumni/status-ajuan3');
+});
 
-// Route::get('/flow', function () {
-//     return view('flow');
-// });
+Route::get('/flow', function () {
+    return view('flow');
+});
 
-// Route::get('/preview-pengajuan', function () {
-//     return view('alumni/preview-pengajuan-legalisir');
-// });
+Route::get('/preview-pengajuan', function () {
+    return view('alumni/preview-pengajuan-legalisir');
+});
 
-// Route::get('/admin', function () {
-//     return view('admin/admin');
-// });
+Route::get('/admin', function () {
+    return view('admin/admin');
+});
 
-// Route::get('/daftar-ajuan-legalisir', function () {
-//     return view('admin/daftar-ajuan-legalisir');
-// });
+Route::get('/daftar-ajuan-legalisir', function () {
+    return view('admin/daftar-ajuan-legalisir');
+});
 
-// Route::get('/index', function () {
-//     return view('index');
-// });
+Route::get('/index', function () {
+    return view('index');
+});
 
-// Route::get('/legalisir-selesai', function () {
-//     return view('admin/legalisir-selesai');
-// });
+Route::get('/legalisir-selesai', function () {
+    return view('admin/legalisir-selesai');
+});
 
-// Route::get('/berkas-selesai', function () {
-//     return view('admin/berkas-selesai');
-// });
+Route::get('/berkas-selesai', function () {
+    return view('admin/berkas-selesai');
+});
 
-// Route::get('/legalisir-pending', function () {
-//     return view('admin/legalisir-pending');
-// });
+Route::get('/legalisir-pending', function () {
+    return view('admin/legalisir-pending');
+});
 
-// Route::get('/legalisir-gagal', function () {
-//     return view('admin/legalisir-gagal');
-// });
+Route::get('/legalisir-gagal', function () {
+    return view('admin/legalisir-gagal');
+});
 
-// Route::get('/berkas-tidak-valid', function () {
-//     return view('admin/berkas-tidak-valid');
-// });
+Route::get('/berkas-tidak-valid', function () {
+    return view('admin/berkas-tidak-valid');
+});
 
-// Route::get('/kuesioner-admin', function () {
-//     return view('admin/kuesioner');
-// });
+Route::get('/kuesioner-admin', function () {
+    return view('admin/kuesioner');
+});
 
-// Route::get('/berkas-pending', function () {
-//     return view('admin/berkas-pending');
-// });
+Route::get('/berkas-pending', function () {
+    return view('admin/berkas-pending');
+});
 
-// Route::get('/edit-ajuan', function () {
-//     return view('admin/edit-ajuan');
-// });
+Route::get('/edit-ajuan', function () {
+    return view('admin/edit-ajuan');
+});
 
-// Route::get('/tambah-kuesioner', function () {
-//     return view('admin/tambah-kuesioner');
-// });
+Route::get('/tambah-kuesioner', function () {
+    return view('admin/tambah-kuesioner');
+});
 
-// Route::get('/edit-kuesioner', function () {
-//     return view('admin/edit-kuesioner');
-// });
+Route::get('/edit-kuesioner', function () {
+    return view('admin/edit-kuesioner');
+});
 
-// Route::get('/validasi-berkas', function () {
-//     return view('admin/validasi-berkas');
-// });
+Route::get('/validasi-berkas', function () {
+    return view('admin/validasi-berkas');
+});
 
-// Route::get('/administrator', function () {
-//     return view('administrator/administrator');
-// });
+Route::get('/administrator', function () {
+    return view('administrator/administrator');
+});
 
-// Route::get('/users', function () {
-//     return view('administrator/users');
-// });
+Route::get('/users', function () {
+    return view('administrator/users');
+});
 
-// Route::get('/user-admin', function () {
-//     return view('administrator/user-admin');
-// });
+Route::get('/user-admin', function () {
+    return view('administrator/user-admin');
+});
 
-// Route::get('/user-alumni', function () {
-//     return view('administrator/user-alumni');
-// });
+Route::get('/user-alumni', function () {
+    return view('administrator/user-alumni');
+});
 
-// Route::get('/daftar-berkas', function () {
-//     return view('admin/daftar-berkas');
-// });
+Route::get('/daftar-berkas', function () {
+    return view('admin/daftar-berkas');
+});
 
-// Route::get('/pertanyaan', function () {
-//     return view('admin/pertanyaan');
-// });
+Route::get('/pertanyaan', function () {
+    return view('admin/pertanyaan');
+});
 
-// Route::get('/pertanyaan-baru', function () {
-//     return view('admin/pertanyaan-baru');
-// });
+Route::get('/pertanyaan-baru', function () {
+    return view('admin/pertanyaan-baru');
+});
 
-// Route::get('respon', function () {
-//     return view('admin/respon');
-// });
+Route::get('respon', function () {
+    return view('admin/respon');
+});
 
-// Route::get('about', function () {
-//     return view('alumni/about');
-// });
-
-// Route::get('profile-admin', function () {
-//     return view('admin/profile-admin');
-// });
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('about', function () {
+    return view('alumni/about');
+});
 
 Route::get('profile-admin', function () {
     return view('admin/profile-admin');
 });
+Auth::routes();
+
+// Route::get('profile-admin', function () {
+//     return view('admin/profile-admin');
+// });
 
 Route::get('/profile-administrator', function () {
     return view('administrator/profile-administrator');
