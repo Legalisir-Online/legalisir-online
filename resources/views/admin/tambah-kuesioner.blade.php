3d85f5836b524ad2c0025aa8eb94f3e7d78a0b24
bachtiarashidiqy <bachtiar.asyidiqy@gmail.com>
2023-07-12 00:49:01 +0700

    Fix tambah-kuesioner blade

--- resources/views/admin/tambah-kuesioner.blade.php
+++ resources/views/admin/tambah-kuesioner.blade.php
@@ -32,32 +32,41 @@
             <div class="row justify-content-center">
                 <div class="col-xs-12">
                     <div class="box" style="background: #D9D9D9; border-radius: 10px; margin-bottom: 30px">
-                        <form class="form-horizontal" style="margin-top: 30px;">
+                        <form class="form-horizontal" style="margin-top: 30px;" method="POST"
+                            action="{{ route('kuesioner.store') }}">
+                            @csrf
                             <div class="form-group">
                                 <label for="inputKuesioner" class="col-xs-2 control-label">Kuesioner</label>
                                 <div class="col-xs-8">
-                                    <input type="kuesioner" class="form-control" id="inputKuesioner">
+                                    <input type="kuesioner" class="form-control" id="inputKuesioner" name="nama">
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="inputPeriode" class="col-xs-2 control-label">Periode</label>
+                                <label for="inputPeriodeMulai" class="col-xs-2 control-label">Periode Mulai</label>
                                 <div class="col-xs-8">
-                                    <input type="periode" class="form-control" id="inputPeriode">
+                                    <input type="date" class="form-control" id="inputPeriodeMulai" name="periode_mulai">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPeriodeSelesai" class="col-xs-2 control-label">Periode Selesai</label>
+                                <div class="col-xs-8">
+                                    <input type="date" class="form-control" id="inputPeriodeSelesai" name="periode_selesai">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="inputStatus" class="col-xs-2 control-label">Status</label>
                                 <div class="col-xs-2">
-                                    <select class="form-control ">
-                                        <option>Aktif</option>
-                                        <option>Tidak Aktif</option>
+                                    <select class="form-control " name="status">
+                                        <option value="1">Aktif</option>
+                                        <option value="0">Tidak Aktif</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="inputPeriode" class="col-xs-2 control-label">Pertanyaan</label>
                                 <div class="col-xs-8">
-                                    <a href="/pertanyaan" class="btn" style="background-color: #404852; color:whitesmoke">
+                                    <a href="/pertanyaan" class="btn"
+                                        style="background-color: #404852; color:whitesmoke">
                                         Buat <i class="fa fa-plus" aria-hidden="true" style="margin-left: 30px"></i>
                                     </a>
                                 </div>
